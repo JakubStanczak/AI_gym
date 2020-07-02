3fe2dc66aedaa18b1dedee9bff8260ae43c98516
Jakub <jakub90stanczak@gmail.com>
2020-07-02 10:29:40 +0200

    Manual play

---
+++ gym_cart_pole.py
@@ -0,0 +1,48 @@
+import gym
+
+env = gym.make('CartPole-v0')
+
+print(env.action_space)
+print(env.observation_space)
+
+def manual_play():
+    observation = env.reset()
+    game_lost = False
+    while not game_lost:
+        env.render()
+
+        key_not_chosen = True
+        while key_not_chosen:
+            action = input('input a for left d for right')
+            if action == 'a':
+                action = 0
+                key_not_chosen = False
+            elif action == 'd':
+                action = 1
+                key_not_chosen = False
+
+        observation, reward, done, info = env.step(action)
+        print('observation, reward, done, info')
+        print(observation, reward, done, info)
+        if done:
+            print('YOU LOST')
+            game_lost = True
+    env.close()
+
+
+
+def run_template():
+    print('sadfas')
+    for i_episode in range(2):
+        observation = env.reset()
+        for t in range(100):
+            env.render()
+            print(observation)
+            action = env.action_space.sample()
+            print(action)
+            observation, reward, done, info = env.step(action)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+    env.close()
+
