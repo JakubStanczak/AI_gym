add212c9e60501cbaf7f275ea63a7af927c34fff
Jakub <jakub90stanczak@gmail.com>
2020-07-09 22:41:08 +0200

    Small improvements added. Only parameter tuning left.

--- gym_lunar_lander.py
+++ gym_lunar_lander.py
@@ -18,31 +18,64 @@ print(env.observation_space.low)
 print(env.action_space.n)
 observation_size = len(env.observation_space.high)
 
+
+# !!!!!!!!!!!!!!!!!! Now model? than iteration 0 model_dir None
+model_iteration = 0
+# model_dir = 'model_weights/model_0_iteration_500_episode'
+model_dir = None
+only_replay = False
+
+
 # initializing model
 model = Sequential([
     Dense(512, input_dim=observation_size, activation='relu'),
     Dropout(0.2),
-    Dense(256, activation='relu'),
-    Dropout(0.2),
-    Dense(128, activation='relu'),
+    Dense(512, activation='relu'),
     Dropout(0.2),
     Dense(env.action_space.n)
 ])
 
-model.compile(loss='mse', optimizer=Adam(lr=0.0005))
+model.compile(loss='mse', optimizer=Adam(lr=0.001))
 # print(model.summary())
 
-episodes = 500
-discount = 0.5
+if model_dir is not None:
+    model.load_weights(model_dir)
+
+def replay():
+    observation = env.reset()
+    env.render()
+    done = False
+    agg_reward = 0
+    while not done:
+        observation = np.reshape(observation, (1, -1))
+        q = model.predict(observation)
+        action = np.argmax(q)
+        # print('q', q)
+        # print('action', action)
+        observation, reward, done, _ = env.step(action)
+        agg_reward += reward
+
+        env.render()
+    print('reward', agg_reward)
+
+if only_replay:
+    while True:
+        replay()
+
+
+episodes = 2_000
+discount = 0.95
 render_every_x_episodes = 100
 
 max_last_actions = 50_000
-use_last_actions = 5_000
+min_last_actions = 5_000
+use_last_actions = 3_000
 observation_memory = deque(maxlen=max_last_actions)
 new_q_memory = deque(maxlen=max_last_actions)
-last_x_scores = deque(maxlen=20)
+not_crashed_memory = deque(maxlen=max_last_actions)
+last_x_scores = deque(maxlen=50)
 
-will_to_explore = 1
+will_to_explore = 0.99
 will_decay_start = episodes // 10
 will_decay_stop = episodes
 will_min = 0.3
@@ -78,9 +111,9 @@ for episode in range(episodes):
         if reward < min_reward: min_reward = reward
         gathered_reward += reward
         new_observation = np.reshape(new_observation, (1, -1))
-        new_q = model.predict(new_observation)
+        next_q = model.predict(new_observation)
         if not done:
-            new_q_value = reward + discount * np.max(new_q)
+            new_q_value = reward + discount * np.max(next_q)
         else:
             new_q_value = reward
         # print('q', q)
@@ -93,7 +126,7 @@ for episode in range(episodes):
         # if done and reward > 100:
         #     print('Tasked solved in episode {} with {} points'.format(episode, reward))
         #     actions_to_solve = actions_taken
-        if done and len(observation_memory) > use_last_actions:
+        if done and len(observation_memory) >= min_last_actions:
             idxs = random.choices(range(len(observation_memory)), k=use_last_actions)
             X = np.array(observation_memory[0])
             y = np.array(new_q_memory[0])
@@ -105,15 +138,16 @@ for episode in range(episodes):
             # print('y', y)
             # print('X shape', X.shape)
             # print('y shape', y.shape)
-            model.fit(X, y, verbose=0, batch_size=64)
+            model.fit(X, y, batch_size=32, epochs=2, verbose=0)
+
 
         if episode % render_every_x_episodes == 0 and episode > will_decay_start:
+            model.save_weights('model_weights/model_{}_iteration_{}_episode'.format(model_iteration, episode))
             env.render()
 
     last_x_scores.append(gathered_reward)
     print('gathered_reward', gathered_reward, 'max_reward', max_reward, 'min_reward', min_reward)
-    print('last_x_scores', np.mean(last_x_scores))
-    # print(Counter(actions_taken))
+    print('last_{}_scores mean'. format(len(last_x_scores)), np.mean(last_x_scores), 'observation_memory_len', len(observation_memory))
     print()
 
     episode_plot.append(episode)
@@ -128,21 +162,9 @@ plt.plot(episode_plot, gathered_reward_plot)
 plt.plot(episode_plot, min_reward_plot)
 plt.plot(episode_plot, max_reward_plot)
 plt.plot(episode_plot, average_reward_per_x_plot)
+plt.savefig('learning_plot.png')
 plt.show()
 
-
-def replay():
-    observation = env.reset()
-    env.render()
-    done = False
-    while not done:
-        observation = np.reshape(observation, (1, -1))
-        q = model.predict(observation)
-        action = np.argmax(q)
-        print('q', q)
-        print('action', action)
-        observation, reward, done, _ = env.step(action)
-        env.render()
 
 while True:
     replay()
